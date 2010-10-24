d1de1dd4bfcdffd05f4fc5c3d9c906b58813e632
insider42 <insider42rus@yandex.ru>
2010-10-24 19:14:16 +0700

    [patch 6] Added scripts for raid instance Vault of Archavon. Patch provided by Lutik and rsa

--- system/ScriptLoader.cpp
+++ system/ScriptLoader.cpp
@@ -316,6 +316,11 @@ extern void AddSC_boss_telestra();
 extern void AddSC_instance_nexus();
 extern void AddSC_boss_sartharion();                        //obsidian_sanctum
 extern void AddSC_instance_obsidian_sanctum();
+extern void AddSC_boss_archavon();                          //vault_of_archavon
+extern void AddSC_boss_emalon();
+extern void AddSC_boss_koralon();
+extern void AddSC_boss_toravon();
+extern void AddSC_instance_vault_of_archavon();
 extern void AddSC_boss_bjarngrim();                         //Ulduar, halls_of_lightning
 extern void AddSC_boss_ionar();
 extern void AddSC_boss_loken();
@@ -740,6 +745,11 @@ void AddScripts()
     AddSC_instance_nexus();
     AddSC_boss_sartharion();                                //obsidian_sanctum
     AddSC_instance_obsidian_sanctum();
+    AddSC_boss_archavon();                                  //vault_of_archavon
+    AddSC_boss_emalon();
+    AddSC_boss_koralon();
+    AddSC_boss_toravon();
+    AddSC_instance_vault_of_archavon();
     AddSC_boss_bjarngrim();                                 //Ulduar, halls_of_lightning
     AddSC_boss_ionar();
     AddSC_boss_loken();
